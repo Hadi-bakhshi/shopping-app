b3dc3384710e064f33656f51095f1c4b1b40f786
hadi <hadibakhshi277@gmail.com>
2022-03-18 13:13:18 +0330

    products added to home and styles added for them

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,24 @@
+import * as data from "../data";
 const HomePage = () => {
-  return <h2>welcome home</h2>;
+  return (
+    <main className="productList--main">
+      <section className="productsList--section">
+        {data.products.map((product) => {
+          return (
+            <section className="product--section">
+              <div className="productImage--container">
+                <img src={product.image} alt={product.name} />
+              </div>
+              <div className="productDescription">
+                <p>{product.name}</p>
+                <p>$ {product.price}</p>
+              </div>
+            </section>
+          );
+        })}
+      </section>
+    </main>
+  );
 };
 
 export default HomePage;
