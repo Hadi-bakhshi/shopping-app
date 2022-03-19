479fe6233e22ea7d48b62cd91f1ba04918505b7b
hadi <hadibakhshi277@gmail.com>
2022-03-19 20:49:23 +0330

    toast notification added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import routes from "./routes";
 import CartProvider from "./context/CartProvider";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <CartProvider>
+      <Toaster />
       <Layout>
         <Switch>
           {routes.map((route, index) => (
