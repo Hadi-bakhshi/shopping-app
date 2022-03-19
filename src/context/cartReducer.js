0c200bc01bdaccab24b87a34dbfc1f771cec9b15
hadi <hadibakhshi277@gmail.com>
2022-03-19 19:53:14 +0330

    actions added

---
+++ src/context/cartReducer.js
@@ -0,0 +1,42 @@
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART": {
+      const updatedCart = [...state.cart];
+      const existingProductIndex = updatedCart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (existingProductIndex < 0) {
+        updatedCart.push({ ...action.payload, quantity: 1 });
+      } else {
+        const updatedItems = { ...updatedCart[existingProductIndex] };
+        updatedItems.quantity++;
+        updatedCart[existingProductIndex] = updatedItems;
+      }
+      return {
+        ...state,
+        cart: updatedCart,
+        total: state.total + action.payload.price,
+      };
+    }
+    case "REMOVE_PRODUCT": {
+      const updatedCart = [...state.cart];
+      const existingProductIndex = updatedCart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      const updatedItems = { ...updatedCart[existingProductIndex] };
+      if (updatedItems.quantity === 1) {
+        const filteredCart = updatedCart.filter(
+          (item) => item.id !== action.payload.id
+        );
+        return { ...state, cart: filteredCart, total: state.total - action.payload.price };
+      } else {
+        updatedItems.quantity--;
+        updatedCart[existingProductIndex] = updatedItems;
+        return { ...state, cart: updatedCart, total: state.total - action.payload.price };
+      }
+    }
+    default:
+      return state;
+  }
+};
+export default cartReducer;
