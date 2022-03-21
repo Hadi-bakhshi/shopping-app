7cf91ef788b2d69cc817e628f76c6af3e31fd8a0
hadi <hadibakhshi277@gmail.com>
2022-03-21 21:08:45 +0330

    routes for signup and login

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import HomePage from "./Pages/HomePage";
 import CartPage from "./Pages/CartPage";
 import Checkout from "./Pages/Checkout";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
 
 const routes = [
+  { path: "/signup", component: SignupPage },
+  { path: "/login", component: LoginPage },
   { path: "/checkout", component: Checkout },
   { path: "/cart", component: CartPage },
   { path: "/", exact: true, component: HomePage },
