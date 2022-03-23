dcfd07c9919047f9940c84cd11c2b0bcaa487405
hadi <hadibakhshi277@gmail.com>
2022-03-23 21:10:29 +0430

    app was wrapped with auth context

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Layout from "./Layout/Layout";
 import routes from "./routes";
 import CartProvider from "./context/CartProvider";
 import { Toaster } from "react-hot-toast";
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
   return (
-    <CartProvider>
-      <Toaster position="top-right" reverseOrder={false} />
-      <Layout>
-        <Switch>
-          {routes.map((route, index) => (
-            <Route key={index} {...route} />
-          ))}
-        </Switch>
-      </Layout>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Toaster position="top-right" reverseOrder={false} />
+        <Layout>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route key={index} {...route} />
+            ))}
+          </Switch>
+        </Layout>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
