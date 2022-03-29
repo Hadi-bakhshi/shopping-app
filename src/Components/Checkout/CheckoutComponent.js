a56ec47fdc91516ee590ccd8c2681bd2719d713c
hadi <hadibakhshi277@gmail.com>
2022-03-29 16:03:30 +0430

    checkout styles and tags updated

--- src/Components/Checkout/CheckoutComponent.js
+++ src/Components/Checkout/CheckoutComponent.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../../context/AuthProvider";
 import { useCart } from "../../context/CartProvider";
+import "./checkOut.css";
 
 const CheckoutComponent = () => {
   const auth = useAuth();
@@ -18,26 +19,41 @@ const CheckoutComponent = () => {
       <section className="cartPageCenter--container">
         {auth ? (
           <section className="cartItem--container">
-            <h3>Checkout details</h3>
-            <p>Name : {auth.name}</p>
-            <p>Email : {auth.email}</p>
-            <p>Phone Number : {auth.phoneNumber}</p>
+            <div className="buyer-info">
+              <h3>Checkout details</h3>
+              <div>
+                <p>Name : </p>
+                <span>{auth.name}</span>
+              </div>
+              <div>
+                <p>Email :</p>
+                <span>{auth.email}</span>
+              </div>
+              <div>
+                <p>Phone Number :</p>
+                <span>{auth.phoneNumber}</span>
+              </div>
+              <div>{new Date().toLocaleString() + ""}</div>
+            </div>
           </section>
         ) : (
           <p>please login to checkout</p>
         )}
-        <section className="cartSummary--container">
+        <section className="cartSummary-container">
+        <h3 className="receiptTitle">Final Receipt</h3>
           {cart &&
             cart.map((item) => {
               return (
-                <div>
+                <div className="finalItems-buy">
+                  
                   {item.name} * {item.quantity} ={" "}
-                  {item.quantity * item.offPrice}
+                 $ {item.quantity * item.offPrice}
                 </div>
               );
             })}
           <hr />
           <div>Total price is :$ {total}</div>
+          <button className="btn primary buyBtn">Buy</button>
         </section>
       </section>
     </main>
